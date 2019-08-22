45d9abd934287dc7f08b5b3883989e16817f8862
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-21 18:34:24 -0700

    add sample test

---
+++ ReactNet/Controllers/PrimeServiceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReactNet.Controllers
+{
+    public class PrimeServiceController : Controller
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first.");
+        }
+
+        public static bool isMagicSquare(List<List<decimal>> matrix)
+        {
+            return true;
+        }
+
+    }
+}
---
+++ ReactNetTest/PrimeServiceControllerTest .cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ReactNet.Controllers
+{
+    [TestFixture]
+    public class PrimeServiceControllerTests
+    {
+        [TestFixture]
+        public class PrimeService_IsPrimeShould
+        {
+            [Test]
+            public void IsPrime_InputIs1_ReturnFalse()
+            {
+                PrimeServiceController primeService = CreatePrimeService();
+                var result = primeService.IsPrime(1);
+
+                Assert.IsFalse(result, "1 should not be prime");
+            }
+
+            [Test]
+            public void When_MatrixIsLessThanThreeByThree_Then_IsMagicSquareFalse()
+            {
+                var matrix = new List<List<decimal>>
+               {
+                   new List<decimal> {1.0m, 2.0m},
+                   new List<decimal> {4.0m, 4.0m}
+               };
+
+                Assert.IsFalse(PrimeServiceController.isMagicSquare(matrix));
+            }
+
+            private PrimeServiceController CreatePrimeService()
+            {
+                return new PrimeServiceController();
+            }
+        }
+    }
+}
+
+//dotnet add package Microsoft.AspNetCore.Mvc.Abstractions --version 2.2.0
+//dotnet add package Microsoft.AspNetCore.Mvc.ViewFeatures --version 2.2.0
