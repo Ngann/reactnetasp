038df0bd8cfea7a054993ac82094e6d778b89abe
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-26 16:28:00 -0700

    save point

--- ReactNet/Controllers/MovieController.cs
+++ ReactNet/Controllers/MovieController.cs
@@ -58,6 +58,20 @@ namespace ReactNet.Controllers
             return movie.Title;
 
         }
+        //possible solution for updat
+        //using(var context = new ...())
+        //    {
+        //        var EditedObj = context
+        //            .Obj
+        //            .Where(x => x. ....)
+        //            .First();
+
+        //        NewObj.Id = EditedObj.Id; //This is important when we first create an object (NewObj), in which the default Id = 0. We can not change an existing key.
+
+        //        context.Entry(EditedObj).CurrentValues.SetValues(NewObj);
+
+        //        context.SaveChanges();
+        //    }
 
         [HttpGet]
         [Route("api/Movie/Delete")]
