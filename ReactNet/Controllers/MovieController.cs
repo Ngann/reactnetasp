df6464ad2aacf3ca741f820f516d3f00a96ca9c0
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-26 14:58:14 -0700

    verify REST endpoints work

--- ReactNet/Controllers/MovieController.cs
+++ ReactNet/Controllers/MovieController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using ReactNet.Data;
 using ReactNet.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ReactNet.Controllers
 {
@@ -29,6 +27,7 @@ namespace ReactNet.Controllers
         }
 
         // POST api/Movie/Create
+        ///api/Movie/Create/?title=test&genre=This&price=1.90&note=somenote&releaseDate=0001-01-01T00
         [HttpGet]
         [Route("api/Movie/Create")]
         public IActionResult Create(Movie info)
@@ -49,7 +48,18 @@ namespace ReactNet.Controllers
             return movieInfo;
         }
 
-        [HttpGet("[action]")]
+        [HttpPut]
+        [Route("api/Movie/Edit")]
+        public int Edit(Movie movie)
+        {
+            _context.Entry(movie).State = EntityState.Modified;
+            _context.SaveChanges();
+            return 1;
+
+        }
+
+        [HttpGet]
+        [Route("api/Movie/Delete")]
         // GET: api/Movie/Delete/{id}
         ///api/Movie/Delete/?id=1
 
@@ -62,10 +72,12 @@ namespace ReactNet.Controllers
             return info;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet]
+        [Route("api/Movie/Search")]
         // GET: api/Movie/Search
+        //api/Movie/Search/?term=Comedy
 
-        public ActionResult<List<Movie>> Search(string term)
+        public List<Movie> Search(string term)
         {
             return _context.Movie
                 .Where(b => b.Title.Contains(term))
